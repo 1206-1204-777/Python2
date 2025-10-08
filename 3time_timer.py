a7e34a1b4e73117a8784f1bbc452865bf942564e
k <xingyanjintian@gmail.com>
2025-10-08 18:51:40 +0900

    コメントの修正

--- 3time_timer.py
+++ 3time_timer.py
@@ -7,7 +7,7 @@ SOUND_FILE = "Flip_Clock01-21(Far-Buzzer-50Hz).mp3" #使用する音声ファイ
 TIMER_SEC = 3 * 60 #タイマーの秒数を設定。今回は3分なので60(1分) x 3でこれを表す
 # Mp3を再生するための事前準備
 pg.mixer.init()
-pg.mixer.music.load(SOUND_FILE) #変数を使い代入されている音声ファイルを読み込む
+pg.mixer.music.load(SOUND_FILE) #指定された音声ファイルを読み込む
 # タイマーのレイアウトを指定
 Layout = [
     [tk.Label("00:00:00", key="-output-",font=("Helvetica",80))],
@@ -32,9 +32,10 @@ while True:
         start_time = da.datetime.now()
     #リセットボタンを押した際の挙動
     if event == "Reset":
-        # start_time変数の値をNoneにして最初の時間(3分)に戻す
+        # タイマーを停止し初期状態に戻す
         start_time = None
         window["-output-"].update("00:00:00")
+        # 音声再生中はそのまま継続
         if pg.mixer.music.get_busy():
             continue
     # タイマーが始まっていないときの処理
@@ -43,7 +44,7 @@ while True:
     # 経過した時間の計算
     now = da.datetime.now() #現在時刻を取得
     delta = now - start_time # 現在時刻からタイマーの開始時間を引き差分を変数に格納
-    #タイマー開始時間から現在時刻の差分が設定した時間(3分)を超えていた場合の処理
+    #タイマー開始時間から現在時刻の差分が設定した時間(3分)に達した場合の処理
     if delta.seconds >= TIMER_SEC: 
         pg.mixer.music.play() # 音楽を再生
         start_time = None # タイマー開始時間をリセット
@@ -52,5 +53,5 @@ while True:
     # 残り時間を表示
     remain = TIMER_SEC - delta.seconds # 設定した時間(3分)からタイマー開始時間と現在時刻との差分を引く
     window["-output-"].update(
-        "0" + str(da.timedelta(seconds=remain)))
+        "0" + str(da.timedelta(seconds=remain))) #冒頭に0をつけるのは時間表示をHH:MM:SSの形式に統一するため
 window.close()
