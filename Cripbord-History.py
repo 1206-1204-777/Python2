3a00928286cdbd6d933b0464a85ee8f8a4448cbf
k <xingyanjintian@gmail.com>
2025-10-12 19:39:50 +0900

    コメントと変数名の修正

--- Cripbord-History.py
+++ Cripbord-History.py
@@ -2,9 +2,9 @@ import pyperclip as pp
 import TkEasyGUI as tk
 
 # 文字列をコピーする
-pp.copy("test") # 現在はハードコーディングだが、実際に使う際はユーザーが入力した値が入る
+pp.copy("test") # 実際に使う際はユーザーが入力した値が入る
 
 # コピーした内容を取得
-test = pp.paste()
-# 取得した内容(text)をポップアップ表示
-tk.popup(test)
+text = pp.paste()
+# 取得した内容(test)をポップアップ表示
+tk.popup(text)
