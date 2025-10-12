31abe666afde2d03b970357092d5a17248a166dc
k <xingyanjintian@gmail.com>
2025-10-12 22:21:36 +0900

    linux対応に修正

--- Cripbord-History.py
+++ Cripbord-History.py
@@ -1,6 +1,11 @@
 import pyperclip as pp
 import TkEasyGUI as tk
-
+import shutil
+# pyperclipが使用するクリップボードツールを明示的に指定
+if shutil.which("xclip"):
+    pp.set_clipboard("xclip")
+elif shutil.which("xsel"):
+    pp.set_clipboard("xsel")
 # 文字列をコピーする
 pp.copy("test") # 実際に使う際はユーザーが入力した値が入る
 
