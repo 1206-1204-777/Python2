08b0e168cd1f4bfc2dc4024b3f8b550f13132eba
k <xingyanjintian@gmail.com>
2025-10-13 15:57:20 +0900

    コメントの修正

--- Cripbord-History.py
+++ Cripbord-History.py
@@ -23,10 +23,10 @@ if os.path.exists(SAVE_FILE): # もしも保存した履歴がある場合の処
 # 履歴を保存する関数の作成
 def save_history(): #関数の宣言
     with open(SAVE_FILE,"w",encoding="utf-8") as f: # 履歴保存ファイルを開く。今回は"w(write、書き込み)を指定し変更できるようにする
-        json.dump(history,f,ensure_ascii=False,indent=2) #Pythonオブジェクト(history配列)をjsonに変換し 履歴保存ファイルに保存
+        json.dump(history,f,ensure_ascii=False,indent=2) #Pythonオブジェクト(history配列)をjsonに変換し履歴保存ファイルに保存
 # 画面に表示する履歴の整形をする関数
 def list_format(history): #関数の宣言と引数にhistoryオブジェクトを指定
 # lambda関数（ラムダ関数、無名関数と呼ばれる簡単な処理を一時的に行うための関数）を使い各処理を定義
-    lf = lambda v: v.strip().replace("\n","")  # 改行コード(\n)を削除して""(空白)に置き換える
-    short = lambda v: v[:20] + "..." if len(v) > 20 else v  # 表示する文字数を20文字までに限定。20文字を超える場合以降の文字を"..."に置き換えて表示(保存した内容が変わるわけではない)
-    return [f"{i+1:02}: {lf(short(h))}" for i, h in enumerate(history)] # 上記で定義したlambda関数を使い整形、履歴にインデックス(行番号)を付与しhistoryに再格納する
+    lf = lambda v: v.strip().replace("\n","")  # 前後の改行コード(\n)を削除して""(空文字)に置き換える
+    short = lambda v: v[:20] + "..." if len(v) > 20 else v  # 表示する履歴を20文字までに限定し超える場合は末尾の文字を"..."に置き換えて表示(保存した内容が変わるわけではない)
+    return [f"{i+1:02}: {lf(short(h))}" for i, h in enumerate(history)] # 上記で定義したlambda関数を使い整形、履歴にインデックス(行番号)を付与しhistoryを返す
