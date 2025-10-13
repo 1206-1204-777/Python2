15ae9e0b8344909fc077d73c9923bb6881475e30
k <xingyanjintian@gmail.com>
2025-10-13 15:13:07 +0900

    一時保存

--- Cripbord-History.py
+++ Cripbord-History.py
@@ -16,11 +16,14 @@ SAVE_FILE = os.path.join(ROOT_DIR,'clipbord-history.json') # 保存するファ�
 MAX_HISTORY = 20
 
 # 既存のクリップボード履歴を読み取る
-history = []
+history = [] # クリップボードの内容を保持する配列
 if os.path.exists(SAVE_FILE): # もしも保存した履歴がある場合の処理
     with open(SAVE_FILE,"r",encoding="utf-8") as f: # 履歴の保存ファイルを開き文字コードの指定をして読み込む （rは読み込む(read)の意味)
         history = json.load(f)
 # 履歴を保存する関数の作成
 def save_history(): #関数の宣言
     with open(SAVE_FILE,"w",encoding="utf-8") as f: # 履歴保存ファイルを開く。今回は"w(write、書き込み)を指定し変更できるようにする
-        json.dump(history,f,ensure_ascii=False,indent=2)
+        json.dump(history,f,ensure_ascii=False,indent=2) #Pythonオブジェクト(history配列)をjsonに変換し 履歴保存ファイルに保存
+#   履歴の整形をする関数
+def list_format(history): #関数の宣言と引数にhistoryオブジェクトを指定
+    lf = lambda v: v.strip().replace("\n","") # 改行コードを"LF"に指定
