300eb0c38d8058e3312477aed6edeb399d1c3185
k <xingyanjintian@gmail.com>
2025-10-13 14:41:49 +0900

    本番用コーディングを開始

--- Cripbord-History.py
+++ Cripbord-History.py
@@ -1,3 +1,5 @@
+import os #OSに関連する機能を使うモジュール
+import json #Pythonオブジェクトとのデータとjsonファイルのデータを変換するモジュール
 import pyperclip as pp
 import TkEasyGUI as tk
 import shutil
@@ -6,10 +8,19 @@ if shutil.which("xclip"):
     pp.set_clipboard("xclip")
 elif shutil.which("xsel"):
     pp.set_clipboard("xsel")
-# 文字列をコピーする
-pp.copy("test") # 実際に使う際はユーザーが入力した値が入る
 
-# コピーした内容を取得
-text = pp.paste()
-# 取得した内容(test)をポップアップ表示
-tk.popup(text)
+# クリップボードの履歴を保存するファイルパスと保存するファイル名を指定
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) # 保存するファイルパスを取得
+SAVE_FILE = os.path.join(ROOT_DIR,'clipbord-history.json') # 保存するファイル名の指定
+# 保存できる履歴の最大数を指定
+MAX_HISTORY = 20
+
+# 既存のクリップボード履歴を読み取る
+history = []
+if os.path.exists(SAVE_FILE): # もしも保存した履歴がある場合の処理
+    with open(SAVE_FILE,"r",encoding="utf-8") as f: # 履歴の保存ファイルを開き文字コードの指定をして読み込む （rは読み込む(read)の意味)
+        history = json.load(f)
+# 履歴を保存する関数の作成
+def save_history(): #関数の宣言
+    with open(SAVE_FILE,"w",encoding="utf-8") as f: # 履歴保存ファイルを開く。今回は"w(write、書き込み)を指定し変更できるようにする
+        json.dump(history,f,ensure_ascii=False,indent=2)
