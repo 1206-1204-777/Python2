20448416def58357821be7f6cbafaf86ae806944
k <xingyanjintian@gmail.com>
2025-10-13 16:25:40 +0900

    コメント修正

--- Cripbord-History.py
+++ Cripbord-History.py
@@ -16,17 +16,17 @@ SAVE_FILE = os.path.join(ROOT_DIR,'clipbord-history.json') # 保存するファ�
 MAX_HISTORY = 20
 
 # 既存のクリップボード履歴を読み取る
-history = [] # クリップボードの内容を保持する配列
+history = [] # クリップボードの内容を保持するリスト
 if os.path.exists(SAVE_FILE): # もしも保存した履歴がある場合の処理
     with open(SAVE_FILE,"r",encoding="utf-8") as f: # 履歴の保存ファイルを開き文字コードの指定をして読み込む （rは読み込む(read)の意味)
         history = json.load(f)
 # 履歴を保存する関数の作成
 def save_history(): #関数の宣言
     with open(SAVE_FILE,"w",encoding="utf-8") as f: # 履歴保存ファイルを開く。今回は"w(write、書き込み)を指定し変更できるようにする
-        json.dump(history,f,ensure_ascii=False,indent=2) #Pythonオブジェクト(history配列)をjsonに変換し履歴保存ファイルに保存
+        json.dump(history,f,ensure_ascii=False,indent=2) #Pythonオブジェクト(historyリスト)をjsonに変換し履歴保存ファイルに保存
 # 画面に表示する履歴の整形をする関数
-def list_format(history): #関数の宣言と引数にhistory配列を指定
+def list_format(history): #関数の宣言と引数にhistoryリストを指定
 # lambda関数（ラムダ関数、無名関数と呼ばれる簡単な処理を一時的に行うための関数）を使い各処理を定義
-    lf = lambda v: v.strip().replace("\n","")  # 改行コード(\n)を含むすべての空白文字(スペースやタブも含む)を削除して""(空文字列)に置き換える
+    lf = lambda v: v.strip().replace("\n","")  # strip関数で前後の空白文字(スペース、タブ)を削除、replace関数で改行コード(\n)を空文字列("")に変更
     short = lambda v: v[:20] + "..." if len(v) > 20 else v  # 表示する履歴が20文字を超える場合、21字目以降の文字を"..."に置き換えて表示(保存した内容が変わるわけではない)
-    return [f"{i+1:02}: {lf(short(h))}" for i, h in enumerate(history)] # 上記で定義したlambda関数を使い整形、履歴にインデックス(行番号)を付与しhistoryを返す
+    return [f"{i+1:02}: {lf(short(h))}" for i, h in enumerate(history)] # 上記で定義したlambda関数を使い整形、履歴にインデックス(行番号)を付与した新しいリストを返す
