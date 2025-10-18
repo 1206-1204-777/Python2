a85231f249f984ce46665e2f2700eed72b1b5b56
k <xingyanjintian@gmail.com>
2025-10-18 14:49:40 +0900

    コメント修正

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 # 1.CSVデータをテキストとして読み込みカンマで区切る  - 不完全な方法
-result = [] # 読み込んだデータを格納する配列reslutを作成
-with open("test.csv","r",encoding= "utf-8") as fp: # 読み込むCSVファイル,読み込み専用として開く,文字エンコードの指定
-    for line in fp.readlines():  #CSVファイルを 1行ずつ読み込む
-        row = line.split(",") # 読み込んだファイル内容をカンマ区切る。ここが不完全な原因
-        result.append(row) # 配列reslutにCSVから読み込んだデータを格納
+result = [] # 読み込んだデータを格納する配列reultを作成
+with open("test.csv","r",encoding= "utf-8") as fp: # CSVファイルをテキストとして読み込み
+    for line in fp.readlines():  # ファイルの各行を順番に読み込み
+        row = line.split(",") # カンマで区切り配列rowに格納
+        result.append(row) # 配列resultにrowを追加
 # データが正しく読み込めたか確認
-peach = result[2] # CSVファイルの3つ目のレコード"桃"の各データを取得できたか確認
-print(f"(1) 果物: {peach[0] } 価格: {peach[2]}")  # 桃レコードの1番目(種類)と3番目(価格)のデータをfフォーマットを使い表示
+peach = result[2] # CSVファイル3行目のレコード、"桃"の各データを取得できたか確認
+print(f"(1) 果物: {peach[0] } 価格: {peach[2]}")  # 桃レコードのindex[0](種類)とindex[2](価格)のデータをfフォーマットを使い表示
